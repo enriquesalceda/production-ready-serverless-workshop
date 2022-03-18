fde576b691b9b00ebac240f4c5d1ef77107f63e3
quique <enriquesalceda@gmail.com>
2022-03-18 15:25:05 +1100

    Multiple fixes:
    - Setup IAM permissions properly on the serverless.yml file.
    - Fix typos and use of variables in serverless.yml file.
    - Fix typos in HTML and Javascript

--- functions/get-restaurants.js
+++ functions/get-restaurants.js
@@ -1,6 +1,5 @@
 const DocumentClient = require('aws-sdk/clients/dynamodb').DocumentClient
 const DynamoDB = new DocumentClient()
-
 const defaultResults = process.env.defaultResults || 8
 const tableName = process.env.restaurants_table
 
@@ -12,14 +11,14 @@ const getRestaurants = async count => {
     Limit: count
   }
 
-  const resp = await dynamodb.scan(req).promise()
-  console.log(`found ${res.Items.length} restaurants`)
+  const resp = await DynamoDB.scan(req).promise()
+  console.log(`found ${resp.Items.length} restaurants`)
 
   return resp.Items
 }
 
 module.exports.handler = async(event, context) => {
-  const restaurant = await getRestaurants(defaultRestaurants)
+  const restaurants = await getRestaurants(defaultResults)
   const response = {
     statusCode: 200,
     body: JSON.stringify(restaurants)
