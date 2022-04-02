11c038171f403400d12ae5029863b1f16bb11a28
quique <enriquesalceda@gmail.com>
2022-04-02 11:57:39 +1100

    Pass the Cognito User Pool id to the index.html

--- functions/get-index.js
+++ functions/get-index.js
@@ -5,6 +5,9 @@ const aws4 = require('aws4')
 const URL = require('url')
 
 const restaurantsApiRoot = process.env.restaurants_api
+const cognitoUserPoolId = process.env.cognito_user_pool_id
+const cognitoClientId = process.env.cognito_client_id
+const awsRegion = process.env.AWS_REGION
 const days = [
   'Sunday',
   'Monday',
@@ -38,6 +41,15 @@ module.exports.handler = async (event, context) => {
   const restaurants = await getRestaurants()
   console.log(`found ${restaurants.length} restaurants`)
   const dayOfWeek = days[new Date().getDay()]
+  const view = {
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientId,
+    dayOfWeek,
+    restaurants,
+    searchUrl: `${restaurantsApiRoot}/search`,
+  }
+
   const html = Mustache.render(template, { dayOfWeek, restaurants })
   const response = {
     statusCode: 200,
